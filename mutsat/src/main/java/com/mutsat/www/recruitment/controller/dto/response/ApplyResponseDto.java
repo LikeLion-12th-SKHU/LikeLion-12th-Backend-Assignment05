47b876e2a01c66fd4ff5c6f583f711dba16c8000
soohyun <sss4920@naver.com>
2024-05-12 00:29:55 +0900

    fix: 반대로 response 나오는 거 수정

--- mutsat/src/main/java/com/mutsat/www/recruitment/controller/dto/response/ApplyResponseDto.java
+++ mutsat/src/main/java/com/mutsat/www/recruitment/controller/dto/response/ApplyResponseDto.java
@@ -3,8 +3,8 @@ package com.mutsat.www.recruitment.controller.dto.response;
 import lombok.Builder;
 
 @Builder
-public record ApplyResponseDto(long recruitmentId, long memberId) {
-	public static ApplyResponseDto of(long recruitmentId, long memberId){
+public record ApplyResponseDto(long memberId,long recruitmentId) {
+	public static ApplyResponseDto of(long memberId, long recruitmentId){
 		return ApplyResponseDto.builder()
 			.memberId(memberId)
 			.recruitmentId(recruitmentId)
